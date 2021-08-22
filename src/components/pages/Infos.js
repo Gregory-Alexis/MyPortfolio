33de07a7a53d123915b53a4867e24cf25028403a
Westindiess <gregoryalexiss@gmail.com>
2021-08-22 10:43:15 +0200

    in progress

--- src/components/pages/Infos.js
+++ src/components/pages/Infos.js
@@ -73,7 +73,10 @@ const Infos = () => {
 					lancer pleinement dans une carrière de développeur front-end React.
 				</p>
 			</div>
-			<div data-aos="flip-down" className="pt-8  m-auto md:w-3/4 lg:w-auto  ">
+			<div
+				data-aos="flip-down"
+				className="pt-8  m-auto md:w-3/4 md:h-3/4 lg:w-auto  "
+			>
 				<img src={ReactIllu} alt="logo react" />
 			</div>
 		</article>
